d438a62f4d77abd6d232d233952481ac9e96382a
Maple <maplevalley8@gmail.com>
2011-11-30 21:17:06 +0800

    update: daemonlize

--- yah3c/yah3c.py
+++ yah3c/yah3c.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style, init
 from h3cpacket import *
 import usermanager
 import plugins
-import os
+import os, sys
 
 __version__ = '0.0.1'
 
@@ -67,6 +67,7 @@ class YaH3C:
             code, id, eap_len = unpack("!BBH", eap_packet[4:8])
             if code == EAP_SUCCESS:
                 display_prompt(Fore.YELLOW, 'Got EAP Success')
+                daemonize('/dev/null','/tmp/daemon.log','/tmp/daemon.log')
             elif code == EAP_FAILURE:
                 display_prompt(Fore.YELLOW, 'Got EAP Failure')
                 print eap_packet[10:].decode('gbk')
@@ -113,7 +114,48 @@ class YaH3C:
             print Fore.RED + Style.BRIGHT + 'Interrupted by user' + Style.RESET_ALL
             self.send_logoff()
 
+def daemonize (stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
 
+    '''This forks the current process into a daemon. The stdin, stdout, and
+    stderr arguments are file names that will be opened and be used to replace
+    the standard file descriptors in sys.stdin, sys.stdout, and sys.stderr.
+    These arguments are optional and default to /dev/null. Note that stderr is
+    opened unbuffered, so if it shares a file with stdout then interleaved
+    output may not appear in the order that you expect. '''
+
+    # Do first fork.
+    try: 
+        pid = os.fork() 
+        if pid > 0:
+            sys.exit(0)   # Exit first parent.
+    except OSError, e: 
+        sys.stderr.write ("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror) )
+        sys.exit(1)
+
+    # Decouple from parent environment.
+    os.chdir("/") 
+    os.umask(0) 
+    os.setsid() 
+
+    # Do second fork.
+    try: 
+        pid = os.fork() 
+        if pid > 0:
+            sys.exit(0)   # Exit second parent.
+    except OSError, e: 
+        sys.stderr.write ("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror) )
+        sys.exit(1)
+
+    # Now I am a daemon!
+    
+    # Redirect standard file descriptors.
+    si = open(stdin, 'r')
+    so = open(stdout, 'a+')
+    se = open(stderr, 'a+', 0)
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
+            
 def prompt_user_info():
     name = raw_input('Input user name: ')
     password = raw_input('Input password: ')
